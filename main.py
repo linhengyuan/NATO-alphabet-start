801b3a1b042092392ab4981a1ebaad34f28479ce
linhengyuan <nono7794050@gmail.com>
2024-09-12 22:39:22 +0800

    TypeError: unhashable type: 'Series'

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
@@ -8,7 +10,6 @@ for (key, value) in student_dict.items():
     #Access key and value
     pass
 
-import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
@@ -20,8 +21,13 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# TODO 1. Create a dictionary in this format:
+# "A": "Alfa", "B": "Bravo"}
+df = pandas.read_csv('nato_phonetic_alphabet.csv')
+print(df)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+test = {row:index for (index, row) in df.iterrows()}
+print(test)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
 
